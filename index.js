1c8d93560ce67ec52658dbdd84b2e4930b893883
NAEL <naelbecaparte3@gmail.com>
2026-05-11 16:30:13 -0300

    webhook funcionando

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
+js
 const express = require("express");
 
 const app = express();
 
 app.use(express.json());
 
-const VERIFY_TOKEN = "meubot123";
-
+// rota inicial
 app.get("/", (req, res) => {
   res.send("Bot online 🚀");
 });
 
+// webhook GET (verificação da Meta)
 app.get("/webhook", (req, res) => {
+  const VERIFY_TOKEN = "meubot123";
+
   const mode = req.query["hub.mode"];
   const token = req.query["hub.verify_token"];
   const challenge = req.query["hub.challenge"];
 
-  if (mode && token === VERIFY_TOKEN) {
-    console.log("Webhook verificado!");
-    return res.status(200).send(challenge);
+  if (mode && token) {
+    if (mode === "subscribe" && token === VERIFY_TOKEN) {
+      console.log("Webhook verificado!");
+      res.status(200).send(challenge);
+    } else {
+      res.sendStatus(403);
+    }
   }
-
-  res.sendStatus(403);
 });
 
+// webhook POST (eventos do Instagram)
 app.post("/webhook", (req, res) => {
   console.log("Evento recebido:");
   console.log(JSON.stringify(req.body, null, 2));
